c46d6cd945256d3547580ee3002cad2fc30119ee
Shen <ihaiyon@gmail.com>
2024-06-22 12:29:09 +0800

    chore: remove GlobalLoadingIndicator for viewport components

--- apps/console/src/components/loading/spinner.tsx
+++ apps/console/src/components/loading/spinner.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
-export const Spinner: React.FC = () => {
+import { cn } from '@ncobase/utils';
+
+interface SpinnerProps {
+  className?: string;
+}
+
+export const Spinner: React.FC<SpinnerProps> = ({ className }) => {
+  const classes = cn(
+    'absolute top-0 right-0 bottom-0 left-0 z-[10000] flex items-center justify-center',
+    className
+  );
+
   return (
-    <div className='flex items-center justify-center h-screen'>
+    <div className={classes}>
       <div className='animate-spin rounded-full h-8 w-8 border-t-4 border-blue-500 border-solid border-transparent'></div>
     </div>
   );
--- apps/console/src/components/viewport/index.tsx
+++ apps/console/src/components/viewport/index.tsx
@@ -1,17 +1,9 @@
 import React, { FC, useEffect } from 'react';
 
 import { isBrowser } from '@ncobase/utils';
-import { useIsFetching } from '@tanstack/react-query';
 import { useLocation, useNavigationType } from 'react-router-dom';
 
-import { Spinner } from '../loading/spinner';
-
 interface ViewportProps extends React.PropsWithChildren {}
-
-const GlobalLoadingIndicator = () => {
-  const isFetching = useIsFetching();
-  return isFetching > 0 ? <Spinner /> : null;
-};
 
 const useScrollToTop = (): void => {
   const { pathname } = useLocation();
@@ -46,6 +38,6 @@ const useFixViewport = (): void => {
 export const Viewport: FC<ViewportProps> = ({ children }) => {
   useScrollToTop();
   useFixViewport();
-  GlobalLoadingIndicator();
+
   return <>{children}</>;
 };
