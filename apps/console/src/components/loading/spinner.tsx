286ac637d974b3188f77d820ba33b02be3ee772e
Shen <ihaiyon@gmail.com>
2024-07-20 10:57:00 +0800

    refactor: adjust spinner styles

--- apps/console/src/components/loading/spinner.tsx
+++ apps/console/src/components/loading/spinner.tsx
@@ -8,13 +8,13 @@ interface SpinnerProps {
 
 export const Spinner: React.FC<SpinnerProps> = ({ className }) => {
   const classes = cn(
-    'absolute top-0 right-0 bottom-0 left-0 z-[10000] flex items-center justify-center',
+    'absolute top-0 right-0 bottom-0 left-0 z-[666] flex items-center justify-center',
     className
   );
 
   return (
     <div className={classes}>
-      <div className='animate-spin rounded-full h-8 w-8 border-t-4 border-blue-500 border-solid border-transparent'></div>
+      <div className='animate-spin rounded-full h-8 w-8  border-slate-300 border-2 border-t-slate-900'></div>
     </div>
   );
 };
